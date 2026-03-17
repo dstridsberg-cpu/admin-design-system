d6bc3c2c336a9f5ca13a3f8a890cbe54eb890a0a
Daniel Stridsberg <dstridsberg@gmail.com>
2026-03-17 18:34:16 +0100

    major change: add design system components and showcase page

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, send_from_directory
+import os
+
+BASE = os.path.dirname(__file__)
+app = Flask(__name__, template_folder='templates', static_folder=BASE, static_url_path='/static')
+
+@app.route('/')
+def showcase():
+    return render_template('showcase.html', section='all')
+
+@app.route('/atoms')
+def atoms():
+    return redirect('/#atoms')
+
+@app.route('/molecules')
+def molecules():
+    return redirect('/#molecules')
+
+@app.route('/organisms')
+def organisms():
+    return redirect('/#organisms')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
